56850406438d8edc9ad6c746e01dfce81b4f0b71
CharlesConley <ccconley2@wisc.edu>
2017-11-28 01:56:33 -0600

    Constructor finished (needs testing)

---
+++ Project4/Project4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project4
+//
+//  Created by Charles Conley on 11/27/17.
+//  Copyright © 2017 Charles Conley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
